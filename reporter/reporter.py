16e2b854485050cf073357415f75c60544e658f0
spydir <d03206642@gmail.com>
2016-03-17 01:15:43 -0400

    push reporter

---
+++ reporter/reporter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys 
+import os 
+import zipfile
+import datetime
+
+def create_doc(template_file, out_file, replaceText):
+    templateDocx = zipfile.ZipFile(template_file)
+    outdir = '/'.join(out_file.split('/')[:-1])
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    newDocx = zipfile.ZipFile(out_file, "w")
+    for file in templateDocx.filelist:
+        content = templateDocx.read(file)
+        for key in replaceText.keys():
+            content = content.replace(str(key), str(replaceText[key]))
+        newDocx.writestr(file.filename, content)
+    templateDocx.close()
+    newDocx.close()
+    
+basepath_in = '/TemplateDir/'
+basepath_out = '/OutputDir/'
+    
+for template_file in os.listdir(basepath_in):
+  if template_file.endswith('.docx'):
+    replaceText = {
+                    "{{AUTHOR}}" : 'Gianluca Nieri',
+                    "{{PID}}" : 'P12345',
+                    "{{PROJECT_TITLE}}" : 'Go to Mars',
+                    "{{PROJECT_AREA}}" : 'Long shots',
+                    "{{PROJECT_DESCRIPTION}}":"Take a ship and go.",
+                    "{{DATE}}" : datetime.datetime.today().strftime("%d/%m/%Y"),
+                    "{{STATUS}}":"FINAL"
+                    }
+    out_file = basepath_out+template_file
+    for key in replaceText.keys():
+        out_file = out_file.replace(str(key), str(replaceText[key]))
+    create_doc(template_file, out_file,replaceText)
